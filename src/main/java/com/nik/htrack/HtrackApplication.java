2d7709577bc42f2ac32e1ca43426c25956f0254b
Milan <milnik79@gmail.com>
2021-10-14 09:03:52 +0200

    Added default users.

--- src/main/java/com/nik/htrack/HtrackApplication.java
+++ src/main/java/com/nik/htrack/HtrackApplication.java
@@ -1,5 +1,9 @@
 package com.nik.htrack;
 
+import com.nik.htrack.domain.Role;
+import com.nik.htrack.domain.User;
+import com.nik.htrack.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,5 +20,20 @@ public class HtrackApplication {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CommandLineRunner run(UserService userService) {
+        return args -> {
+            userService.saveRole(new Role(null, "ADMIN"));
+            userService.saveRole(new Role(null, "USER"));
+
+            userService.saveUser(new User(null, "Test", "Admin", "ta@example.com", "1234"));
+            userService.saveUser(new User(null, "Test", "User", "tu@example.com", "1234"));
+
+            userService.addRoleToUser("ta@example.com", "ADMIN");
+            userService.addRoleToUser("ta@example.com", "USER");
+            userService.addRoleToUser("tu@example.com", "USER");
+        };
     }
 }
